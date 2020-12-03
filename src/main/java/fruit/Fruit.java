62c05233c3c4d411570af060c4fa367e0d9aabb1
Valeriy <simha88@yandex.ru>
2020-12-03 13:56:15 +0000

    Added set of fruits

---
+++ src/main/java/fruit/Fruit.java
@@ -0,0 +1,11 @@
+package fruit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Fruit {
+    private String name;
+    private double weight;
+}
---
+++ src/main/java/fruit/Main.java
@@ -0,0 +1,30 @@
+package fruit;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit apple = new Fruit("Яблоко", 0.8);
+        Fruit apricot = new Fruit("Абрикос", 0.3);
+        Fruit fig = new Fruit("Инжир", 0.05);
+        Fruit orange = new Fruit("Апельсин", 0.3);
+        Fruit kiwi = new Fruit("Киви", 0.075);
+
+        Set<Fruit> fruits = new HashSet<>(Arrays.asList(apple, apricot, fig, orange, kiwi));
+        System.out.println(fruits);
+
+        fruits.add(new Fruit("Груша", 0.15));
+        System.out.println(fruits);
+
+        fruits.add(new Fruit("Киви", 0.075));
+        System.out.println(fruits); //не добавилось
+
+        fruits.remove(fig);
+        System.out.println(fruits);
+
+        System.out.println("fruits.contains(apple) = " + fruits.contains(apple));
+        System.out.println("fruits.contains(fig) = " + fruits.contains(fig));
+    }
+}
